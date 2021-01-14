b1ba16fac06130799c24ec237ee0b72917bfdb09
sanazez <sanazez1234@gmail.com>
2021-01-14 14:15:07 +0300

    feat: add router '/'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  request({ url: `https://jobs.github.com/positions`, json: true }, function (err, response, body) {
+    res.json(body);
+  });
 });
 
 module.exports = router;
+
--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/', function (req, res, next) {
-    request({ url: `https://jobs.github.com/positions.json?page=1&search=${req.query.search}`, json: true }, function (err, response, body) {
+router.get('/search', function (req, res, next) {
+    request({ url: `https://jobs.github.com/positions.json?page=0&search=${req.query.search}`, json: true }, function (err, response, body) {
         res.json(body);
     });
 });
