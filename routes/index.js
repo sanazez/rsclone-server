7b4c9351aa7d9963e024b51ea34640b79ecfdee2
sanazez <sanazez1234@gmail.com>
2021-01-17 14:38:30 +0300

    fix: change api-url for search

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function (req, res, next) {
-  request({ url: `https://remotive.io/api/remote-jobs?limit=5`, json: true }, function (err, response, body) {
+  request({ url: `https://remotive.io/api/remote-jobs?limit=10`, json: true }, function (err, response, body) {
     res.json(body);
   });
 });
--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/search', function (req, res, next) {
-    request({ url: `https://jobs.github.com/positions.json?page=0&search=${req.query.search}`, json: true }, function (err, response, body) {
+    request({ url: `https://remotive.io/api/remote-jobs?search=${req.query.search}`, json: true }, function (err, response, body) {
         res.json(body);
     });
 });
