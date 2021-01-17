c2fac2f021ed4157b5eb2973398cc8217b02681c
sanazez <sanazez1234@gmail.com>
2021-01-17 13:24:47 +0300

    feat: change url api-remotive

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function (req, res, next) {
-  request({ url: `https://jobs.github.com/positions`, json: true }, function (err, response, body) {
+  request({ url: `https://remotive.io/api/remote-jobs?limit=5`, json: true }, function (err, response, body) {
     res.json(body);
   });
 });
